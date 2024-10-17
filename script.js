fb1e094c3cda63bbedc5a733685af79b628b56c8
Kheenan Walkins <17203992+xerotolerant@users.noreply.github.com>
2024-10-17 09:06:02 -0400

    Add basic operations

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// operations
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+function divide(a, b) {
+    return a / b;
+
+}
