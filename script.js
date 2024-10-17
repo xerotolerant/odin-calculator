aab35e7f34a79ef6ec384ab525c9d90019f0f1a8
Kheenan Walkins <17203992+xerotolerant@users.noreply.github.com>
2024-10-17 09:27:30 -0400

    Add display

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// variables
+let firstNum;
+let secondNum;
+
+function operate(a, operation, b) {
+    switch (operation) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
 
 // operations
 function add(a, b) {
